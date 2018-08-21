5d47e0c34736026361fe850f733bac390dded6d6
enderluffy <34403788+enderluffy@users.noreply.github.com>
2018-08-21 18:59:53 +0200

    index.tpl.php

---
+++ themes/dungeon/overrides/widgets/user/viewsindex.tpl.php
@@ -0,0 +1,17 @@
+<form action="<?php echo url('user/login'); ?>" method="post">
+	<div class="form-group">
+		<input type="text" class="form-control" name="<?php echo $data['form_id']; ?>[login]" placeholder="Identifiant" />
+	</div>
+	<div class="form-group">
+		<input type="password" class="form-control" name="<?php echo $data['form_id']; ?>[password]" placeholder="Mot de passe" />
+	</div>
+	<div class="text-right">
+		<input type="hidden" name="<?php echo $data['form_id']; ?>[redirect]" value="<?php echo $this->url->request; ?>" />
+		<input type="hidden" name="<?php echo $data['form_id']; ?>[remember_me][]" value="on" />
+	</div>
+	<div class="text-right">
+		<input type="submit" class="btn btn-connexion btn-cennexion" value="Connexion" />
+		<a href="<?php echo url('user'); ?>" class="btn btn-inscrire" data-toggle="tooltip" title="S'inscrire sur le site"><i class="fa fa-sign-in"></i></a>
+		<a href="<?php echo url('user/lost-password'); ?>" class="btn btn-question" data-toggle="tooltip" title="Mot de passe perdu ?"><i class="fa fa-question"></i></a>
+	</div>
+</form>
